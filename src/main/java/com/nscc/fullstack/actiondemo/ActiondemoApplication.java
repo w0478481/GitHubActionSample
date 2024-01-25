0537aaf447c09c20dbf010050e4118484a8548f6
W0478481 <adetulafemi5@gmail.com>
2024-01-25 09:19:31 -0400

    exercise software

---
+++ src/main/java/com/nscc/fullstack/actiondemo/ActiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.nscc.fullstack.actiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiondemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nscc/fullstack/actiondemo/ActiondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nscc.fullstack.actiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiondemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
